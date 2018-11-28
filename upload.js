4259af21a967bb21fa1d848d22961f029c299967
SassanKermani <SassanKermani2@gmail.com>
2018-11-27 19:52:35 -0700

    ok hear gose hopeing

---
+++ upload.js
@@ -0,0 +1,272 @@
+/*============================
+=            vars            =
+============================*/
+
+let fs = require(`fs`);
+let vids = [];
+let foldor = ``;
+let isNewFoldor = false;
+
+/*=====  End of vars  ======*/
+
+/*========================================
+=            selecting folder            =
+========================================*/
+
+let populateFoldors = ()=>{
+	fs.readdir(`db`, (err, dbObjectsNames)=>{
+		// console.log(dbObjectsNames);
+		for(let i = 0; i < dbObjectsNames.length; i++){
+			// console.log(i);
+			populateFoldorsPartTwoAddingButtons(dbObjectsNames[i]);
+		}
+		let addNewFoldor = document.createElement(`button`);
+		addNewFoldor.innerHTML = `make new foldor`;
+		addNewFoldor.addEventListener(`click`, ()=>{
+			// populateFoldorsPartTwoAddingButtons(document.getElementById(`nameOfNewFoldor`).value);
+			let tempButton = document.createElement(`button`);
+			tempButton.innerHTML = document.getElementById(`nameOfNewFoldor`).value;
+			tempButton.addEventListener(`click`, ()=>{
+				foldor = document.getElementById(`nameOfNewFoldor`).value;
+				isNewFoldor = true;
+
+			});
+			document.getElementById(`listOffolors`).appendChild(tempButton);
+			foldor = document.getElementById(`nameOfNewFoldor`).value;
+			isNewFoldor = true;
+		});
+		document.getElementById(`newFolderSection`).appendChild(addNewFoldor);
+	});
+}
+
+let populateFoldorsPartTwoAddingButtons = (dbObjectName)=>{										//activly working on this
+	let tempButton = document.createElement(`button`);
+	tempButton.innerHTML = dbObjectName;
+	tempButton.addEventListener(`click`, ()=>{
+		foldor = dbObjectName;
+		isNewFoldor = false;
+		getEpisodesFromDb(foldor).then((moreUsefullData)=>{
+			console.log(moreUsefullData);
+			for(let i = 0; i < moreUsefullData.vids.length; i++){
+				// console.log()
+				vids.push(new vidObj(moreUsefullData.vids[i], false, `./vids`));
+			}
+			addVidArrToPage();
+		});
+	});
+	document.getElementById(`listOffolors`).appendChild(tempButton);
+}
+
+/*=====  End of selecting folder  ======*/
+
+/*===============================================
+=            Adding Episodes To Page            =
+===============================================*/
+
+let getEpisodesFromDb = (nameOfShow)=>{
+	
+	let tempPormis = new Promise((resolve, reject)=>{
+		fs.readFile(`./db/${nameOfShow}`, (err, data)=>{
+			let moreUsefullData = JSON.parse(data);
+			// console.log(moreUsefullData);
+			resolve(moreUsefullData);
+		});
+	});
+	return tempPormis;
+}
+
+let getEpisodesFromSelect = ()=>{
+	let newEpisodes = document.getElementById(`inputNewEpisodes`).files;
+	// console.log(newEpisodes);
+	for(let i = 0; i < newEpisodes.length; i++){
+		vids.push(new vidObj(newEpisodes[i].name, true, newEpisodes[i].path));
+	}
+	console.log(vids);
+}
+
+let addVidArrToPage = ()=>{
+
+	while (document.getElementById(`episodesSection`).firstChild) {
+		document.getElementById(`episodesSection`).removeChild(document.getElementById(`episodesSection`).firstChild);
+	}
+
+	for(let i = 0; i < vids.length; i++){
+		let tempP = document.createElement(`p`);
+		tempP.innerHTML = vids[i].name;
+		
+		//buttons
+		let buttonsOne = document.createElement(`button`)
+		let buttonsTwo = document.createElement(`button`)
+		let buttonsThree = document.createElement(`button`)
+		let buttonsFour = document.createElement(`button`)
+
+		buttonsOne.addEventListener(`click`, ()=>{
+			vids = arrMoveIndexToTop(vids, i);
+			addVidArrToPage();
+		});
+		buttonsTwo.addEventListener(`click`, ()=>{
+			vids = arrMoveIndexToBottom(vids, i);
+			addVidArrToPage();
+		});
+		buttonsThree.addEventListener(`click`, ()=>{
+			vids = arrMoveIndexOneUp(vids, i);
+			addVidArrToPage();
+		});
+		buttonsFour.addEventListener(`click`, ()=>{
+			vids = arrMoveIndexOneDown(vids, i);
+			addVidArrToPage();
+		});
+
+		buttonsOne.innerHTML = `top`;
+		buttonsTwo.innerHTML = `bottom`;
+		buttonsThree.innerHTML = `up`;
+		buttonsFour.innerHTML = `down`;
+
+		document.getElementById(`episodesSection`).appendChild(tempP);
+
+		tempP.appendChild(buttonsOne);
+		tempP.appendChild(buttonsTwo);
+		tempP.appendChild(buttonsThree);
+		tempP.appendChild(buttonsFour);
+		// unOrderdList.appendChild(tempLi);
+	}
+}
+
+
+/*----------  Utility  ----------*/
+let vidObj = function (name, isNew, location){
+	this.name = name;
+	this.isNew = isNew;
+	this.location = location;
+}
+
+/*=====  End of Adding Episodes To Page  ======*/
+
+/*====================================
+=            Wrting to db            =
+====================================*/
+
+let writeToDb = ()=>{
+	if(foldor.length != 0 & vids.length != 0){
+		let tempObj = {
+			vids : vids.map(x => x.name)
+		};
+
+		fs.writeFile( `./db/${foldor}`, JSON.stringify(tempObj), (err)=>{
+			if(err){throw err};
+			for(let i = 0; i < vids.length; i++){
+				if(vids[i].isNew){
+					fs.createReadStream(vids[i].location).pipe(fs.createWriteStream(`./vids/${vids[i].name}`));
+				}
+			}
+		});
+
+		console.log(tempObj);
+
+	}else{
+		console.log(`fix the writeToDb`);
+	}
+}
+
+/*=====  End of Wrting to db  ======*/
+
+
+/*=====================================
+=            Arr functions            =
+=====================================*/
+
+let arrMoveIndexToTop = (arr, index)=>{
+	let newArr = [];
+	newArr[0] = arr[index];
+	for(let i = 0; i < arr.length; i++){
+		if(i < index){
+			newArr[i + 1] = arr[i];
+		}
+		if(i == index){
+			// newArr[i] = newThing;
+		}
+		if(i > index){
+			newArr[i] = arr[i];
+		}
+	}
+	return(newArr);
+};
+
+let arrMoveIndexToBottom = (arr, index)=>{
+	let newArr = [];
+	for(let i = 0; i < arr.length; i++){
+		if(i < index){
+			newArr[i] = arr[i];
+		}
+		if(i == index){
+			// newArr[i] = newThing;
+		}
+		if(i > index){
+			newArr[i - 1] = arr[i];
+		}
+	}
+	newArr[arr.length - 1] = arr[index];
+	return(newArr);
+};
+
+let arrMoveIndexOneUp = (arr, index)=>{
+	if(index > 0){
+		newArr = [];
+		for(let i = 0; i < arr.length; i++){
+			if(i == index ){
+				newArr[i] = arr[i - 1];
+			}
+			if(i == index - 1){
+				newArr[i] = arr[i + 1];
+			}
+			if(i != index && i != index - 1){
+				// console.log(i);
+				newArr[i] = arr[i];
+			}
+		}
+		return(newArr);
+	}else{
+		return(arr);
+	}
+};
+
+let arrMoveIndexOneDown = (arr, index)=>{
+	if(index >= 0 && index < arr.length){
+		newArr = [];
+		for(let i = 0; i < arr.length; i++){
+			if(i == index ){
+				newArr[i] = arr[i + 1];
+			}
+			if(i == index + 1){
+				newArr[i] = arr[i - 1];
+			}
+			if(i != index && i != index + 1){
+				// console.log(i);
+				newArr[i] = arr[i];
+			}
+		}
+		return(newArr);
+	}else{
+		return(arr);
+	}
+};
+
+/*=====  End of Arr functions  ======*/
+
+
+/*===============================
+=            Testing            =
+===============================*/
+
+populateFoldors();
+
+document.getElementById(`importSectionGoButton`).addEventListener(`click`, ()=>{
+	getEpisodesFromSelect ();
+	addVidArrToPage();
+});
+
+document.getElementById(`uploadButton`).addEventListener(`click`, ()=>{
+	writeToDb();
+	// console.log(vids);
+})
+/*=====  End of Testing  ======*/
---
+++ video.js
@@ -0,0 +1,253 @@
+/*==============================
+=            Set Up            =
+==============================*/
+
+let fs = require('fs');
+let episodes = [];
+
+let currentShow;
+let currentEpisode;
+
+let speed = document.getElementById(`videoPlayer`).playbackRate;
+let volume = document.getElementById(`videoPlayer`).volume;
+let muteTureFalse = false;
+
+/*=====  End of Set Up  ======*/
+
+/*===============================================
+=            Adding Videos Functions            =
+===============================================*/
+
+
+let getShows = ()=>{
+	fs.readdir(`./db`, (err, dbFileNames)=>{
+		// console.log(dbFileNames);
+		for(let i = 0; i < dbFileNames.length; i++){
+			let tempButton = document.createElement(`button`);
+			tempButton.innerHTML = dbFileNames[i];
+			tempButton.addEventListener(`click`, ()=>{
+				// console.log(`test`);
+				getEpisodesForShow(dbFileNames[i])
+				currentShow = dbFileNames[i];
+				document.getElementById(`showThatIsPlaying`).innerHTML = `Show : ${dbFileNames[i]}`
+			});
+			document.getElementById(`showsDiv`).appendChild(tempButton);
+		}
+	});
+}
+
+let getEpisodesForShow = (showName)=>{
+	fs.readFile(`./db/${showName}`, (err, data)=>{
+		// console.log(JSON.parse(data));
+		episodes = JSON.parse(data).vids;
+		// console.log(episodes);
+		addEpisodesToPage()
+	});
+}
+
+let addEpisodesToPage = ()=>{
+	while (document.getElementById(`episodes`).firstChild) {
+		document.getElementById(`episodes`).removeChild(document.getElementById(`episodes`).firstChild);
+	}
+	for(let i = 0; i < episodes.length; i++){
+		let tempButton = document.createElement(`button`);
+		tempButton.innerHTML = episodes[i];
+		tempButton.addEventListener(`click`, ()=>{
+			changeEpisode(episodes[i]);
+			// document.getElementById(`episodeThatIsPlaying`).innerHTML = `Episode : ${episodes[i]}`
+			updateContinue();
+		});
+		document.getElementById(`episodes`).appendChild(tempButton);
+	}
+}
+
+let changeEpisode = (newEpisode)=>{
+	let tempVid = document.getElementById(`videoPlayer`);
+	tempVid.src = `./vids/${newEpisode}`;
+	tempVid.load();
+	document.getElementById(`episodeThatIsPlaying`).innerHTML = `Episode : ${newEpisode}`
+	currentEpisode = newEpisode;
+
+}
+
+let updateContinue = ()=>{
+	// continue
+	fs.writeFile(`./continue.json`, JSON.stringify({show : currentShow, episode : currentEpisode}), (err)=>{
+		if(err){console.log(err);}
+	});
+}
+
+let loadContinue = ()=>{
+	fs.readFile(`./continue.json`, (err, json)=>{
+		let moreUseFullJson = JSON.parse(json);
+		currentShow = moreUseFullJson.show;
+		document.getElementById(`showThatIsPlaying`).innerHTML = `Show : ${moreUseFullJson.show}`
+		currentEpisode = moreUseFullJson.episode;
+		fs.readFile(`./db/${currentShow}`, (err, data)=>{
+		// console.log(JSON.parse(data));
+			episodes = JSON.parse(data).vids;
+			// console.log(episodes);
+			addEpisodesToPage()
+			changeEpisode(currentEpisode);
+		});
+	})
+}
+
+/*=====  End of Adding Videos Functions  ======*/
+
+/*===============================
+=            Hotkeys            =
+===============================*/
+
+/*----------  To make things easer  ----------*/
+Object.defineProperty(HTMLMediaElement.prototype, 'playing', {
+	get: function(){
+		return !!(this.currentTime > 0 && !this.paused && !this.ended && this.readyState > 2);
+	}
+});
+
+/*----------  Functions  ----------*/
+let nextEpisode = ()=>{
+	// console.log(`episodes.indexOf(currentEpisode) : ${episodes.indexOf(currentEpisode)}`);
+	// console.log(`episodes.length : ${episodes.length}`);
+	if(episodes.indexOf(currentEpisode) < episodes.length - 1){
+		changeEpisode(episodes[episodes.indexOf(currentEpisode) + 1]);
+	}else{
+		changeEpisode(episodes[0]);
+	}
+	fixVideo();
+}
+
+let lastEpisode = ()=>{
+	if(episodes.indexOf(currentEpisode) > 0){
+		changeEpisode(episodes[episodes.indexOf(currentEpisode) - 1]);
+	}else{
+		changeEpisode(episodes[episodes.length - 1]);
+	}
+	fixVideo();
+}
+
+let setSpeed = (num)=>{
+	if(num >= .5 && num <= 3){
+		document.getElementById(`videoPlayer`).playbackRate = num;
+		document.getElementById(`SpeedUiBit`).innerHTML = document.getElementById(`videoPlayer`).playbackRate;
+		speed = num;
+	}
+}
+
+let pausePlay = ()=>{
+	if(document.getElementById(`videoPlayer`).playing == true){
+		document.getElementById(`videoPlayer`).pause()
+	}else{
+		document.getElementById(`videoPlayer`).play();
+	}
+}
+
+let mute = ()=>{
+	if(document.getElementById(`videoPlayer`).muted != true){
+		document.getElementById(`videoPlayer`).muted = true;
+		muteTureFalse = true;
+	}else{
+		document.getElementById(`videoPlayer`).muted = false;
+		muteTureFalse = false;
+	}
+}
+
+let ajustVolume = (num)=>{
+	if(num >= 0 && num <= 1){
+		document.getElementById(`videoPlayer`).volume = num;
+		volume = num;
+	}
+}
+
+let skipForward = ()=>{
+	if( document.getElementById("videoPlayer").currentTime + 10 > document.getElementById("videoPlayer").duration ){
+		document.getElementById("videoPlayer").currentTime = document.getElementById("videoPlayer").duration;
+	}else{
+		document.getElementById("videoPlayer").currentTime = document.getElementById("videoPlayer").currentTime + 10;
+	}
+}
+
+let skipBackward = ()=>{
+	if( document.getElementById("videoPlayer").currentTime - 10 < 0 ){
+		document.getElementById("videoPlayer").currentTime = 0;
+	}else{
+		document.getElementById("videoPlayer").currentTime = document.getElementById("videoPlayer").currentTime - 10;
+	}
+}
+
+/*----------  Utility  ----------*/
+let fixVideo = ()=>{
+	// console.log(`speed : ${speed}`);
+	// console.log(`volume : ${volume}`);
+	// console.log(`muteTureFalse : ${muteTureFalse}`);
+
+	setSpeed(speed);
+	ajustVolume(volume);
+	document.getElementById(`videoPlayer`).muted = muteTureFalse;
+}
+
+/*----------  tieing them to the keys  ----------*/
+window.addEventListener('keypress', function(keypressThing){
+	console.log(keypressThing.key);
+	if(keypressThing.key == `k`){
+		pausePlay();
+	}
+
+	if(keypressThing.key == `l`){
+		skipForward();
+	}
+
+	if(keypressThing.key == `j`){
+		skipBackward();
+	}
+
+	if(keypressThing.key == `m`){
+		mute();
+	}
+
+	if(keypressThing.key == `n`){
+		ajustVolume(document.getElementById(`videoPlayer`).volume + .2);
+	}
+
+	if(keypressThing.key == `b`){
+		ajustVolume(document.getElementById(`videoPlayer`).volume - .2);
+	}
+
+	if(keypressThing.key == `p`){
+		setSpeed(document.getElementById(`videoPlayer`).playbackRate + .25);
+	}
+
+	if(keypressThing.key == `o`){
+		setSpeed(document.getElementById(`videoPlayer`).playbackRate - .25);
+	}
+
+	if(keypressThing.key == `i`){
+		nextEpisode();
+	}
+
+	if(keypressThing.key == `u`){
+		lastEpisode();
+	}
+
+});
+
+/*=====  End of Hotkeys  ======*/
+
+/*===============================
+=            Testing            =
+===============================*/
+
+getShows();
+
+// document.getElementById(`continue`).addEventListener(`click`, loadContinue);
+
+// document.getElementById(`continue`).addEventListener(`click`, ()=>{
+// 	// console.log( document.getElementById(`videoPlayer`).playing );
+// 	// console.log( episodes.indexOf(currentEpisode) );
+// 	loadContinue();
+// 	// fixVideo();
+// });
+loadContinue();
+
+/*=====  End of Testing  ======*/
